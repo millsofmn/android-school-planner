209f9e4b74a5caca96b867da39c5480e9ae60f2f
Lyle Miller <miller.lyle@mayo.edu>
2019-08-29 18:33:49 -0500

    added mentor activity

--- app/src/main/java/com/millsofmn/android/schoolplanner/MainActivity.java
+++ app/src/main/java/com/millsofmn/android/schoolplanner/MainActivity.java
@@ -1,14 +1,45 @@
 package com.millsofmn.android.schoolplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button btnNavMentors;
+    private Button btmNavMangeTerm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnNavMentors = findViewById(R.id.btn_nav_mentor);
+        btmNavMangeTerm = findViewById(R.id.btn_nav_terms);
+
+        btnNavMentors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Going to Mentors", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), Mentors.class);
+                startActivity(intent);
+            }
+        });
+
+        btmNavMangeTerm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Going to Terms", Toast.LENGTH_SHORT).show();
+//                Intent intent = new Intent(getApplicationContext(), TermsListActivity.class);
+//                startActivity(intent);
+            }
+        });
     }
 }
